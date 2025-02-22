2e2d6dfe665921f7ab3edbbabeaab71b6c3b9743
Adan <aguilarend@gmail.com>
2025-02-22 11:03:18 -0600

    Add files via upload
    Acabe la tarea

---
+++ holamundo.cpp
@@ -0,0 +1,22 @@
+// Esta es la biblioteca de entrada y 
+// salida de datos.
+#include <iostream>
+
+/*
+ TODO LO 
+QUE 
+ESTE AQUI
+ES UN COMENTARIO
+*/
+
+
+int main( int argc, char **argv ){
+	std::cout << "Hola Mundo!";
+	return 0;
+}
+
+
+
+
+
+
---
+++ imprimirvalores.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+	//String es cadena de caracteres
+	std::string nombre="Juan";
+	// int es enteros abrv. Integer
+	int edad=34;
+
+	std::cout << "El nombre es " << nombre <<" de edad " << edad;
+	return 0;
+}
